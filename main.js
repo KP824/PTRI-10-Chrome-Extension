535c29d72cdb709a295298a1ae2ed701f3be95d1
kpedu824 <kp.edu824@gmail.com>
2023-01-20 10:43:33 -0800

    update 1.20.23

---
+++ main.js
@@ -0,0 +1,30 @@
+// When user starts extension, have to select which offerings they want today
+// checkboxes / buttons for each: jokes, koans, exercises
+// set interval for occurrences
+// submit button
+// (non-user, in background) disable any not checked
+// Option to reset / stop the extension (clear interval)
+// Show start pop-up again, with checkboxes
+
+/* Notes for Jokes
+- show in extension pop-up
+- programming, foodie, one-liners, self-deprecating
+*/
+// Create an array and insert 20 random jokes, iterate over array and display curr joke
+// STRETCH GOAL: have joke var, call joke API, store returned joke in var, display var; sometime before next occurence, call API again for new joke
+
+/* Notes for Koans
+- define what koans are (storytime!)
+- show in extension pop-up, be prepared to be enlightened!
+*/
+
+/* Notes for Health Reminders
+- get up and stretch, yoga stretch with image / GIF of how to do it
+- maybe open a new tab (warn user - if select for health reminders, will force open new tab)
+*/
+
+/* IF WE HAD MORE TIME... HOW WOULD WE IMPROVE EXTENSION?
+- Maybe create tutorial with preselected joke/koan/ or exercise to demonstrate functionality
+- therapy API integration
+- koan/s stories more interactive
+*/
