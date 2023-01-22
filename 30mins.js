35e7298df481b1b9f6fa9c433240bd69f4be25a9
kpedu824 <kp.edu824@gmail.com>
2023-01-22 11:10:45 -0800

    Working Demogit add .! v1.2git add .!

--- 30mins.js
+++ 30mins.js
@@ -1,10 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
+  // 30 minutes interval
   runAll();
+  setInterval(runAll, 300000);
 
   /*************************END OF  DOM EVENT LOADED*************************** */
 });
-
-// MAIN FUNCTION
 
 function runAll() {
   fetch(
@@ -90,59 +90,8 @@ function runAll() {
     return Math.floor(Math.random() * 47);
   })();
 
+  // Appending to element inner text
   document.getElementById(
     "positive-quote"
   ).innerText = `${affirmations[randomAffIndex]}`;
 }
-
-// const btn1 = document.getElementById('newTabBtn1');
-
-// btn1.addEventListener('click', )
-
-// const btn2 = document.getElementById('newTabBtn2');
-// btn2.addEventListener('click', () => {});
-
-// When user starts extension, have to select which offerings they want today
-// checkboxes / buttons for each: jokes, koans, exercises
-// set interval for occurrences
-// submit button
-// (non-user, in background) disable any not checked
-// Option to reset / stop the extension (clear interval)
-// Show start pop-up again, with checkboxes
-
-/* Notes for Jokes
-- show in extension pop-up
-- programming, foodie, one-liners, self-deprecating
-*/
-// Create an array and insert 20 random jokes, iterate over array and display curr joke
-// STRETCH GOAL: have joke var, call joke API, store returned joke in var, display var; sometime before next occurence, call API again for new joke
-
-/* Notes for Koans
-- define what koans are (storytime!)
-- show in extension pop-up, be prepared to be enlightened!
-*/
-
-/* Notes for Health Reminders
-- get up and stretch, yoga stretch with image / GIF of how to do it
-- maybe open a new tab (warn user - if select for health reminders, will force open new tab)
-*/
-
-/* IF WE HAD MORE TIME... HOW WOULD WE IMPROVE EXTENSION?
-- Maybe create tutorial with preselected joke/koan/ or exercise to demonstrate functionality
-- therapy API integration
-- koan/s stories more interactive
-*/
-
-/********************  N I C E  B L O C K  ****************** */
-/* Start of affirmation API */
-
-// fetch('https://www.affirmations.dev/', {
-//   headers: {
-//     "Accept": "application/json",
-//     "mode": 'no-cors',
-//     'Access-Control-Allow-Origin': '*'
-//   }
-// })
-// .then((response) => response.json())
-// .then((data) => console.log(data));
-// console.log(`We here!`);
